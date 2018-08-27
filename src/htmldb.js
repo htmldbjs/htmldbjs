3bf13388db8be1383f7a5f169db74d03f64b082f
Aykut Aydınlı <aykut@pyronome.com>
2018-08-27 16:43:47 +0300

    A bug in HTMLDB.checkTableValidation method fixed

--- src/htmldb.js
+++ src/htmldb.js
@@ -225,7 +225,7 @@ var HTMLDB = {
             validation = validations[i];
             if (HTMLDB.getHTMLDBParameter(validation, "table")
             		== tableElement.getAttribute("id")) {
-            	currentResponse = checkTableValidation(
+            	currentResponse = HTMLDB.checkTableValidation(
             			tableElement,
             			object,
             			validation);
